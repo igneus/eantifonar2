24710ad0d364ebd401519ccf7cd0acc867cac1ea
Jakub Pavlik <severus@post.cz>
2022-08-31 22:00:37 +0200

    is able to load and display antiphon images

--- README.md
+++ README.md
@@ -11,9 +11,15 @@ For now this extension has only been published as source on Github.
 In order to install it from source please follow the official instructions
 https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Temporary_Installation_in_Firefox
 
+This early development version requires
+
+- [adiutor][adiutor] with data properly imported and running at http://localhost:3000
+- `security.mixed_content.block_active_content` Firefox configuration option set to false
+
 ## License
 
 GNU/GPL 3
 
 [ia]: http://www.inadiutorium.cz/
 [ean]: https://github.com/igneus/eantifonar
+[adiutor]: https://github.com/igneus/adiutor
--- extension.js
+++ extension.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const apiEndpoint = 'http://localhost:3000/api/eantifonar/search'; // TODO replace with public API
+
 // TODO for debugging only
 document.body.prepend(document.createTextNode('eantifonar loaded ======='));
 
@@ -13,20 +15,97 @@ const doXPath = (xpath) => {
     }
 
     return r;
-}
+};
+
+// returns text contained directly in the node, ignoring text content of child elements
+const nodeOwnText = (node) => {
+    const clone = node.cloneNode(true);
+    clone.childNodes.forEach((child) => {
+        if (child.nodeType != Node.TEXT_NODE) {
+            clone.removeChild(child);
+        }
+    });
+
+    return clone.innerText.trim().replace(/\s+/g, ' ');
+};
+
+const responsoryText = (node) => {
+    return nodeOwnText(node.children[0]) + ' V. ' + nodeOwnText(node.children[2]);
+};
+
+const highlight = (element, colour) => element.style.border = '2px solid ' + colour;
+
+const loadChants = (query, callback) => {
+    let payload = {};
+    query.forEach((item, i) => payload[i.toString()] = item);
+
+    fetch(apiEndpoint, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(payload)
+    })
+        .then((response) => {
+            if (response.status != 200) {
+                console.log(response);
+                return {};
+            }
+
+            return response.json();
+        })
+        .then(callback)
+        .catch(console.error);
+};
+
+const addChantsToElements = (elements, responseData) => {
+    elements.forEach((chantText, i) => {
+        let options = responseData[i.toString()];
+        if (null === options) {
+            return;
+        }
+
+        let chantData = options[0];
+        let img = document.createElement('img');
+        img.setAttribute('src', chantData.image);
+        img.setAttribute('alt', chantData.lyrics);
+
+        let parent = chantText.parentNode;
+        parent.insertBefore(img, chantText.nextSibling);
+    });
+};
+
+let elements = [];
+let query = [];
 
 // invitatory; repetition of an antiphon after a psalm
 doXPath("//p[./span[@class='red' and contains(text(), 'Ant.')]]")
-    .forEach((antiphon) => antiphon.style.border = '2px solid red');
+    .forEach((element) => {
+        highlight(element, 'orange');
+        elements.push(element);
+        query.push({lyrics: nodeOwnText(element)});
+    });
 
 // psalm antiphons
 doXPath("//p[./span[@class='red' and contains(text(), 'ant.')]]")
-    .forEach((antiphon) => antiphon.style.border = '2px solid red');
+    .forEach((element) => {
+        highlight(element, 'red');
+        elements.push(element);
+        query.push({lyrics: nodeOwnText(element)});
+    });
 
 // short responsory
 doXPath("//div[@class='respons' and count(./p) > 2]")
-    .forEach((antiphon) => antiphon.style.border = '2px solid blue');
+    .forEach((element) => {
+        highlight(element, 'blue');
+        elements.push(element);
+        query.push({lyrics: responsoryText(element)});
+    });
 
 // Gospel antiphon
 doXPath("//p[./span[@class='red' and contains(text(), 'Antifona k')]]")
-    .forEach((antiphon) => antiphon.style.border = '2px solid yellow');
+    .forEach((element) => {
+        highlight(element, 'yellow');
+        elements.push(element);
+        query.push({lyrics: nodeOwnText(element)});
+    });
+
+loadChants(query, (responseData) => addChantsToElements(elements, responseData));
