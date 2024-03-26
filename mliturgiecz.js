42a3d3b076bdd043205e2139002c868551d1aa71
Jakub Pavlik <severus@post.cz>
2024-03-26 17:27:23 +0100

    m.liturgie.cz: basic support
    to see how many missal antiphons can be found in the LOTH
    for a Graduale-simplex-like Mass proper

---
+++ mliturgiecz.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const lang = 'cs';
+
+const elementGroups = [
+    {
+        desc: 'introit',
+        xpath: "//h4[contains(text(), 'Vstupní antifona')]/following-sibling::p[1]",
+        highlight: 'orange'
+    },
+    {
+        desc: 'responsorial psalm',
+        xpath: "//h4[./a[@name='Mezizpev']]/following-sibling::p[1]",
+        highlight: 'red'
+    },
+    {
+        desc: 'alleluia / verse before the Gospel',
+        xpath: "//h4[./a[@name='Pred_evangeliem']]/following-sibling::p[1]",
+        highlight: 'blue'
+    },
+    {
+        desc: 'communio',
+        xpath: "//h4[contains(text(), 'Antifona k přijímání')]/following-sibling::p[1]",
+        highlight: 'yellow'
+    }
+];
+
+class MLiturgieCzEAntiphonal extends EAntiphonal {
+    decorateChantText(node, group) {
+        if (this.debug) {
+            node.style.border = '2px solid ' + group.highlight;
+        }
+    }
+
+    decorateChantTextNotationMissing(node) {
+        node.setAttribute('class', node.getAttribute('class') + ' eantifonar-music-not-found');
+    }
+}
+
+
+new MLiturgieCzEAntiphonal(lang, elementGroups).run();
