565a864653bd88f64c330d5603e1fef34a0e1f48
Jakub Pavlik <severus@post.cz>
2024-04-14 17:48:45 +0200

    m.liturgie.cz: don't attempt to load music for alleluia
    Although some alleluia verses share text with Office antiphons, they are
    really a different genre. Even for purposes of a "Graduale simplex"
    it makes no sense to use an Office antiphon (even if the text matched
    completely) as an alleluia.

--- mliturgiecz.js
+++ mliturgiecz.js
@@ -14,11 +14,13 @@ const elementGroups = [
         xpath: "//h4[contains(string(.), 'Mezizpěv')]/following-sibling::p[1]",
         highlight: 'red'
     },
+    /*
     {
         desc: 'alleluia / verse before the Gospel',
         xpath: "//h4[contains(string(.), 'Zpěv před evangeliem')]/following-sibling::p[1]",
         highlight: 'blue'
     },
+    */
     {
         desc: 'communio',
         xpath: "//h4[contains(text(), 'Antifona k přijímání')]/following-sibling::p[1]",
